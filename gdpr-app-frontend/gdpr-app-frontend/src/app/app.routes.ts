95bf3c18e9fce4c0dc07543c44ff84b35b4a5cc6
badisSaada <la228671@student.helha.be>
2025-07-31 12:24:32 +0200

    create guard

--- gdpr-app-frontend/gdpr-app-frontend/src/app/app.routes.ts
+++ gdpr-app-frontend/gdpr-app-frontend/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  // Redirection par défaut vers login
+  
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   
-  // Route pour le login
+  
   { path: 'login', component: LoginComponent },
   
-  // Route wildcard pour les pages non trouvées (optionnel)
+  
   { path: '**', redirectTo: '/login' }
 ];
