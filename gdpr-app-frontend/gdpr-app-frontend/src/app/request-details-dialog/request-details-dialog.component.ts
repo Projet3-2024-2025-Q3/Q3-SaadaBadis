6ec9758635f0c667576d67684cba5552f6dd4d20
badisSaada <la228671@student.helha.be>
2025-08-08 12:16:59 +0200

    details component

---
+++ gdpr-app-frontend/gdpr-app-frontend/src/app/request-details-dialog/request-details-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request-details-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-details-dialog.component.html',
+  styleUrl: './request-details-dialog.component.css'
+})
+export class RequestDetailsDialogComponent {
+
+}
