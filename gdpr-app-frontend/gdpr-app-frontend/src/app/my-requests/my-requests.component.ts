43d2633e40387fcb66828daf6ecb555e7cf39219
badisSaada <la228671@student.helha.be>
2025-08-08 12:45:46 +0200

    update details html

--- gdpr-app-frontend/gdpr-app-frontend/src/app/my-requests/my-requests.component.ts
+++ gdpr-app-frontend/gdpr-app-frontend/src/app/my-requests/my-requests.component.ts
@@ -362,7 +362,7 @@ export class MyRequestsComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   createNewRequest(): void {
-    this.router.navigate(['/create-request']);
+    this.router.navigate(['/create']);
   }
 
   refreshData(): void {
