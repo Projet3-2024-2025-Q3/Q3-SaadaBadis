d6163e23571090430511901116aa95253e45be98
badisSaada <la228671@student.helha.be>
2025-08-09 11:23:15 +0200

    manager-navbar component

---
+++ gdpr-app-frontend/gdpr-app-frontend/src/app/manager-navbar/manager-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './manager-navbar.component.html',
+  styleUrl: './manager-navbar.component.css'
+})
+export class ManagerNavbarComponent {
+
+}
