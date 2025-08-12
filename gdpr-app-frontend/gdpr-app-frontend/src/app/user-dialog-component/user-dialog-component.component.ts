8c1205fc18a133a99f3c16aa096f8471f01a5832
badisSaada <la228671@student.helha.be>
2025-08-12 14:34:53 +0200

    create user-dialog-component

---
+++ gdpr-app-frontend/gdpr-app-frontend/src/app/user-dialog-component/user-dialog-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-dialog-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-dialog-component.component.html',
+  styleUrl: './user-dialog-component.component.css'
+})
+export class UserDialogComponentComponent {
+
+}
